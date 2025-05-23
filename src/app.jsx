78469bb095a10bb6ec93d4ba8b91242a009cf9e8
meokisama <hoangdinhsang123@gmail.com>
2025-05-23 16:53:37 +0700

    add more shadcn ui

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
 import { Toaster } from "@/components/ui/sonner";
 
 export function App() {
   return (
     <div>
-      <Button>Click me to show toast</Button>
       <Toaster />
     </div>
   );
