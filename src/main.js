ade27cd3da9a44341e028e627990f52b1e3127b7
meokisama <hoangdinhsang123@gmail.com>
2025-06-18 21:34:34 +0700

    feat: implement auto update

--- src/main.js
+++ src/main.js
@@ -32,6 +32,9 @@ import {
   getClock,
 } from "./services/rcu-controller.js";
 import dgram from "dgram";
+import { updateElectronApp } from "update-electron-app";
+
+updateElectronApp()
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
