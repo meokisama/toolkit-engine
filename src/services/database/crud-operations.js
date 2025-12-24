d466cd631fe3d63b06f28cd8b092465ff2f8f350
meokisama <hoangdinhsang123@gmail.com>
2025-12-24 15:50:04 +0700

    fix schedule

--- src/services/database/crud-operations.js
+++ src/services/database/crud-operations.js
@@ -328,14 +328,33 @@ const tableConfigs = {
     },
     buildUpdateQuery(tableName, itemData, id) {
       // Build dynamic update query based on itemData fields
-      const allowedFields = ["name", "address", "description", "source_unit",
-        "color1", "color2", "color3", "color4", "color5", "color6", "color7", "color8",
-        "color9", "color10", "color11", "color12", "color13", "color14", "color15", "color16"];
+      const allowedFields = [
+        "name",
+        "address",
+        "description",
+        "source_unit",
+        "color1",
+        "color2",
+        "color3",
+        "color4",
+        "color5",
+        "color6",
+        "color7",
+        "color8",
+        "color9",
+        "color10",
+        "color11",
+        "color12",
+        "color13",
+        "color14",
+        "color15",
+        "color16",
+      ];
 
       const fieldsToUpdate = [];
       const params = [];
 
-      allowedFields.forEach(field => {
+      allowedFields.forEach((field) => {
         if (itemData.hasOwnProperty(field)) {
           fieldsToUpdate.push(`${field} = ?`);
           params.push(itemData[field]);
@@ -493,7 +512,6 @@ export const crudOperations = {
 
         // Update scene items if address changed
         if (currentItem.address !== itemData.address) {
-          console.log(`Updating scene items for curtain ${id}: ${currentItem.address} -> ${itemData.address}`);
           this.updateSceneItemsAddress(id, tableName, itemData.address);
         }
 
